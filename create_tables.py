7d125434efb05785c6d198161ab809c9ed7c9d52
mdeghady <mostafadeghady22@gmail.com>
2022-10-13 21:30:31 +0200

    Creating & Dropping tables quries

--- create_tables.py
+++ create_tables.py
@@ -3,7 +3,7 @@ from sql_quries import create_table_quries , drop_table_quries
 
 def create_db():
     try:
-        conn = psycopg2.connect("dbname = postgres user = postgres password=mostafa1208")
+        conn = psycopg2.connect("dbname = postgres user = postgres password=")
     except psycopg2.Error as e:
         print("cannot connect to database")
         print(e)
